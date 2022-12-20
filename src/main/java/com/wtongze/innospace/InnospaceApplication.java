f4597b37bf0d96700ac2213e71dcb884e34e04a4
Tongze Wang <wtongze@gmail.com>
2022-12-20 05:50:20 -0800

    feat: init commit

---
+++ src/main/java/com/wtongze/innospace/InnospaceApplication.java
@@ -0,0 +1,13 @@
+package com.wtongze.innospace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnospaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnospaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wtongze/innospace/InnospaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtongze.innospace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnospaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
